49c2859891c7f845b1dec33e67198cd20bc51623
caspar d <cappetjatte@gmail.com>
2026-08-03 11:48:41 +0000

    feat: set polybars to each monitor

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
-
 # Launch a bar on each monitor
 for m in $(polybar --list-monitors | cut -d":" -f1); do
   case "$m" in
+  HDMI-A-0) bar="caspar-hdmi" ;;
+  DisplayPort-0) bar="caspar-dp" ;;
   DP-1) bar="caspar-dp1" ;;
   eDP-1) bar="caspar-edp1" ;;
   *) bar="caspar" ;;
@@ -16,5 +16,4 @@ for m in $(polybar --list-monitors | cut -d":" -f1); do
   polybar "$bar" 2>&1 | tee -a /tmp/polybar-$m.log &
   disown
 done
-
 echo "Bars launched..."
