82d2950845003bc1adca04272f9710620f231ddb
caspar d <caspar.danielsson@surkat.se>
2026-08-18 08:17:12 +0200

    feat[polybar]: change gitstatus to only change color of git icon

--- scripts/scripts/polybar-github-status.sh
+++ scripts/scripts/polybar-github-status.sh
@@ -4,9 +4,13 @@ indicator=$(jq -r '.status.indicator' <<<"$status")
 description=$(jq -r '.status.description' <<<"$status")
 
 case "$indicator" in
-none) echo "%{F#a6da95} GitHub: $description%{F-}" ;;     # green, operational
-minor) echo "%{F#eed49f} GitHub: $description%{F-}" ;;    # yellow
-major) echo "%{F#f5a97f} GitHub: $description%{F-}" ;;    # orange
-critical) echo "%{F#ed8796} GitHub: $description%{F-}" ;; # red
-*) echo "%{F#6e738d} GitHub?%{F-}" ;;                     # unknown/curl fail
+# none) echo "%{F#a6da95}   󰔓 %{F-}" ;;     # green, operational
+# minor) echo "%{F#eed49f}    %{F-}" ;;    # yellow
+# major) echo "%{F#f5a97f}   󰔑 %{F-}" ;;    # orange
+# critical) echo "%{F#ed8796}    %{F-}" ;; # red
+none) echo "%{F#a6da95}  %{F-}" ;;     # green, operational
+minor) echo "%{F#eed49f}  %{F-}" ;;    # yellow
+major) echo "%{F#f5a97f}  %{F-}" ;;    # orange
+critical) echo "%{F#ed8796}  %{F-}" ;; # red
+*) echo "%{F#6e738d} GitHub?%{F-}" ;;   # unknown/curl fail
 esac
